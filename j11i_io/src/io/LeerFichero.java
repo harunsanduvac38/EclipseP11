f0d6528b1efd18750a4b3201e30c148b24451d4e
mananas <mananas@10.1.2.15>
2025-07-11 11:27:48 +0200

    sdf

--- j11i_io/src/io/LeerFichero.java
+++ j11i_io/src/io/LeerFichero.java
@@ -10,9 +10,30 @@ public class LeerFichero {
 	public static void main(String[] args) {
 		String fichero = "/Users/mananas/ContactoDaoMem.java";
 		
-		System.out.println(muestraFichero(fichero));
+//		System.out.println(muestraFichero(fichero));
+		
+		System.out.println(leerFichero(fichero));
 	}
 	
+	
+	
+	public static String leerFichero(String fichero) {
+		StringBuilder sb = new StringBuilder();
+		
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(fichero))) {  //Java automaticamente cierra br, no hace falta escribir la parte finally para cerrarlo
+			String linea;
+			while ((linea = br.readLine()) != null) {
+				sb.append(linea + "\n");
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+		
+		return sb.toString();
+		
+	}
 	
 
 	
